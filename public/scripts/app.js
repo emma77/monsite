bde51c6dd1064277ae01388243dd3d7c535a7150
emma77 <eballois@aliceadsl.fr>
2012-10-11 18:15:49 +0200

    creation de menu fixe

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,15 +1,14 @@
 function check_name(name){
-	if( name == "gabriel"){
+	if(name == "gabriel"){
 		return"Win !";
-	}else if( name == "clement"){
-    	return"We find clement";
+	}else if(name == "clement"){
+    	return "We find clement";
     }else{
 		var res = "truth" + name + "is different from gabriel and clement";
 		return res;
 	}
 }
 
-$(document).ready(function(){
-	alert(check_name(" emma "));
-});
+
+
 
